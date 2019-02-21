8eee6aa8d2523457fbf30edae2932ef8683a9f8b
kasarda <fkasarda@gmail.com>
2019-02-21 19:05:08 +0100

    Init

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { NativeRouter, Route } from 'react-router-native'
+import Dashboard from './app/Dashboard'
+
+
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello</Text>
-      </View>
-    );
+      <NativeRouter>
+        <View>
+          <Route exact path="/" component={Dashboard} />
+        </View>
+      </NativeRouter>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
